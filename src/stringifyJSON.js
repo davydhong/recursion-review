cf4bedcfd1486abd0ff78c5814b7886a2fadcca0
davydhong <davydhong@gmail.com>
2018-07-30 12:36:54 -0700

    completed stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,48 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'boolean' || typeof obj === 'number') {
+    var res = obj.toString();
+  } else if (obj === null || obj === undefined) {
+    var res = '' + obj + '';
+  } else if (typeof obj === 'string') {
+    var res = '"' + obj + '"';
+  } else if (typeof obj === 'function') {
+    var res = '{}';
+  } else if (typeof obj === 'object') {
+    if (Array.isArray(obj)) { //Array if True
+      var inside = [];
+      obj.forEach( function(element) {
+        inside.push(stringifyJSON(element));
+      });
+      var res = '[' + inside + ']';
+
+    
+      // return '[' + res + ']';
+    } else { //Object if False
+      var inside = [];
+      for (key in obj) {
+        
+
+        if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+          var value = obj[key];
+          var keyStr = stringifyJSON(key);
+          var valStr = stringifyJSON(value);
+          var pair = keyStr + ':' + valStr;
+          inside.push(pair);
+        }
+
+        
+
+      }
+      var res = '{' + inside + '}';
+    }
+  }
+  console.log(res);
+
+
+  //answer cheat
+  // ans = JSON.stringify(obj);
+  // console.log('cheat:' + ans);
+  return  res;
 };
