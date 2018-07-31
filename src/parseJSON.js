04415089c32227b3bdebdb92e0e11b989ea1cd14
davydhong <davydhong@gmail.com>
2018-07-30 21:06:32 -0700

    half way through parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,8 +3,24 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
+  console.log(json);
+  console.log(JSON.parse(json));
+  return JSON.parse(json);
+
   if (!_.contains(json, '{')) {
     return eval(json);
-  }
-  // return JSON.parse(json);
+  } else {
+    var obj = {};
+    var inner = json.slice(1, json.length - 1);
+    if (inner.length > 0 ) {
+      var arr = inner.split(',');
+      for (var i = 0; i < arr.length; i++) {
+        var pairArr = arr[i].split(':');
+        var key = pairArr[0];
+        var val = pairArr[1];
+        obj[parseJSON(key)] = parseJSON(val);
+      }
+    } 
+    return obj;
+  }  
 };
